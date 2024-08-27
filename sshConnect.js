830a20eb333e8204503afd9a3f6daef8e5bd8c1b
Danish Shakeel <danishshakeel54@gmail.com>
2024-08-27 17:35:17 +0200

    feat: remove hardcoded values

--- sshConnect.js
+++ sshConnect.js
@@ -1,18 +1,44 @@
-import fs from "fs";
-import inquirer from "inquirer";
-import { spawn } from "child_process";
-import Table from "cli-table3";
-import autocompletePrompt from "inquirer-autocomplete-prompt";
+import fs from 'fs';
+import path from 'path';
+import inquirer from 'inquirer';
+import { spawn } from 'child_process';
+import Table from 'cli-table3';
+import autocompletePrompt from 'inquirer-autocomplete-prompt';
 
-inquirer.registerPrompt("autocomplete", autocompletePrompt);
+inquirer.registerPrompt('autocomplete', autocompletePrompt);
 
-const filePath = "/Users/danishshakeel/Custom Scripts/data/ssh.json";
+const filePath = path.join(process.cwd(), 'data', 'ssh.json');
+
+function ensureDirectoryExists() {
+  const directory = path.dirname(filePath);
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+}
 
 function readData(callback) {
-  fs.readFile(filePath, "utf8", (err, data) => {
+  ensureDirectoryExists();
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      const defaultContent = JSON.stringify({});
+      fs.writeFile(filePath, defaultContent, 'utf8', (writeErr) => {
+        if (writeErr) {
+          console.error(`Error creating file at [${filePath}]: ${writeErr}`);
+          return;
+        }
+        readFileContent(callback);
+      });
+    } else {
+      readFileContent(callback);
+    }
+  });
+}
+
+function readFileContent(callback) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(
-        `An error was encountered while reading the file at [${filePath}]: ${err}`
+        `An error was encountered while reading the file at [${filePath}]: ${err}`,
       );
       return;
     }
@@ -21,20 +47,21 @@ function readData(callback) {
 }
 
 function writeData(data) {
+  ensureDirectoryExists();
   fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
     if (err) {
-      console.error("An error occurred during file writing:", err);
+      console.error('An error occurred during file writing:', err);
     }
   });
 }
 
-function formatChoiceOptions(data, filter = "") {
+function formatChoiceOptions(data, filter = '') {
   const keys = Object.keys(data).filter((key) =>
-    key.toLowerCase().includes(filter.toLowerCase())
+    key.toLowerCase().includes(filter.toLowerCase()),
   );
   const maxLabelLength = Math.max(
     ...keys.map((key) => key.length),
-    "Label".length
+    'Label'.length,
   );
 
   return keys.map((key) => {
@@ -50,25 +77,25 @@ function formatChoiceOptions(data, filter = "") {
 
 function generateTable(data) {
   const chars = {
-    top: "═",
-    "top-mid": "╤",
-    "top-left": "╔",
-    "top-right": "╗",
-    bottom: "═",
-    "bottom-mid": "╧",
-    "bottom-left": "╚",
-    "bottom-right": "╝",
-    left: "║",
-    "left-mid": "╟",
-    mid: "─",
-    "mid-mid": "┼",
-    right: "║",
-    "right-mid": "╢",
-    middle: "│",
+    top: '═',
+    'top-mid': '╤',
+    'top-left': '╔',
+    'top-right': '╗',
+    bottom: '═',
+    'bottom-mid': '╧',
+    'bottom-left': '╚',
+    'bottom-right': '╝',
+    left: '║',
+    'left-mid': '╟',
+    mid: '─',
+    'mid-mid': '┼',
+    right: '║',
+    'right-mid': '╢',
+    middle: '│',
   };
 
   const table = new Table({
-    head: ["Label", "Username", "Hostname"],
+    head: ['Label', 'Username', 'Hostname'],
     chars: chars,
   });
 
@@ -83,19 +110,19 @@ function addEntry() {
   inquirer
     .prompt([
       {
-        type: "input",
-        name: "key",
-        message: "Enter the label:",
+        type: 'input',
+        name: 'key',
+        message: 'Enter the label:',
       },
       {
-        type: "input",
-        name: "username",
-        message: "Username:",
+        type: 'input',
+        name: 'username',
+        message: 'Username:',
       },
       {
-        type: "input",
-        name: "hostname",
-        message: "Hostname (IP address):",
+        type: 'input',
+        name: 'hostname',
+        message: 'Hostname (IP address):',
       },
     ])
     .then((answers) => {
@@ -105,7 +132,7 @@ function addEntry() {
           hostname: answers.hostname,
         };
         writeData(data);
-        console.log("The connection was added successfully.");
+        console.log('The connection was added successfully.');
         listConnections();
       });
     });
@@ -116,23 +143,23 @@ function removeEntry() {
     inquirer
       .prompt([
         {
-          type: "autocomplete",
-          name: "key",
-          message: "Select the connection you wish to remove:",
+          type: 'autocomplete',
+          name: 'key',
+          message: 'Select the connection you wish to remove:',
           source: (_answers, input) =>
             Promise.resolve(formatChoiceOptions(data, input)),
         },
         {
-          type: "confirm",
-          name: "confirm",
-          message: "Are you sure you want to remove this connection?",
+          type: 'confirm',
+          name: 'confirm',
+          message: 'Are you sure you want to remove this connection?',
         },
       ])
       .then((answer) => {
         if (!answer.confirm) return;
         delete data[answer.key];
         writeData(data);
-        console.log("The connection was removed successfully.");
+        console.log('The connection was removed successfully.');
         listConnections();
       });
   });
@@ -143,22 +170,22 @@ function modifyEntry() {
     inquirer
       .prompt([
         {
-          type: "autocomplete",
-          name: "key",
-          message: "Select the connection to modify:",
+          type: 'autocomplete',
+          name: 'key',
+          message: 'Select the connection to modify:',
           source: (answers, input) =>
             Promise.resolve(formatChoiceOptions(data, input)),
         },
         {
-          type: "input",
-          name: "newUsername",
-          message: "New username:",
+          type: 'input',
+          name: 'newUsername',
+          message: 'New username:',
           default: (answers) => data[answers.key].username,
         },
         {
-          type: "input",
-          name: "newHostname",
-          message: "New hostname (IP address):",
+          type: 'input',
+          name: 'newHostname',
+          message: 'New hostname (IP address):',
           default: (answers) => data[answers.key].hostname,
         },
       ])
@@ -168,23 +195,14 @@ function modifyEntry() {
           hostname: answers.newHostname,
         };
         writeData(data);
-        console.log("The connection was modified successfully.");
+        console.log('The connection was modified successfully.');
       });
   });
 }
 
 function listConnections() {
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error(
-        `An error was encountered while reading the file at [${filePath}]: ${err}`
-      );
-      return;
-    }
-    const connections = JSON.parse(data);
-
-    // Log the table with data
-    console.log(generateTable(connections));
+  readData((data) => {
+    console.log(generateTable(data));
   });
 }
 
@@ -193,9 +211,9 @@ function sshIntoHost() {
     inquirer
       .prompt([
         {
-          type: "autocomplete",
-          name: "key",
-          message: "Choose the host connection you wish to SSH into:",
+          type: 'autocomplete',
+          name: 'key',
+          message: 'Choose the host connection you wish to SSH into:',
           source: (answers, input) =>
             Promise.resolve(formatChoiceOptions(data, input)),
         },
@@ -203,30 +221,34 @@ function sshIntoHost() {
       .then((answers) => {
         const entry = data[answers.key];
         console.log(
-          `Attempting to connect: ssh ${entry.username}@${entry.hostname}`
+          `Attempting to connect: ssh ${entry.username}@${entry.hostname}`,
         );
-        const sshProcess = spawn(
-          "ssh",
-          [`${entry.username}@${entry.hostname}`],
-          { stdio: "inherit" }
-        );
+        spawn('ssh', [`${entry.username}@${entry.hostname}`], {
+          stdio: 'inherit',
+        });
       });
   });
 }
 
+function showConfigLocation() {
+  console.log(`Config file location: ${filePath}`);
+}
+
 function mainMenu() {
   const args = process.argv.slice(2);
-  if (args.includes("--add")) {
+  if (args.includes('--add')) {
     addEntry();
-  } else if (args.includes("--remove")) {
+  } else if (args.includes('--remove')) {
     removeEntry();
-  } else if (args.includes("--modify")) {
+  } else if (args.includes('--modify')) {
     modifyEntry();
-  } else if (args.includes("--list")) {
+  } else if (args.includes('--list')) {
     listConnections();
+  } else if (args.includes('--config-location')) {
+    showConfigLocation();
   } else {
     sshIntoHost();
   }
 }
 
-mainMenu();
+export { mainMenu };
